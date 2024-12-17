f7fbb373ae48e7cf2eefbfdde67a6d472e6453f1
sergeiryzhkov <sergeyryzhkov2001@mail.ru>
2024-12-18 01:58:21 +0300

    Alert dialog box was created

--- app/src/main/java/com/example/testapp/ui/screens/bottom/SettingsScreen.kt
+++ app/src/main/java/com/example/testapp/ui/screens/bottom/SettingsScreen.kt
@@ -6,9 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,6 +38,8 @@ import kotlinx.coroutines.launch
 fun SettingsScreen(
     dataStoreManager: DataStoreManager,
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+
     ChangeStatusBarColor(color = WhiteColor, isIconsLight = false)
     Column(
         modifier = Modifier
@@ -83,12 +91,34 @@ fun SettingsScreen(
                 iconTint = RedColor,
                 text = "Reset progress",
                 onClick = {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        dataStoreManager.clearSelectedLessons()
-                    }
+                    showDialog = true
                 }
             )
         }
+    }
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = { showDialog = false },
+            title = { Text(text = "Confirm Reset") },
+            text = { Text("Are you sure you want to reset the progress? This action cannot be undone.") },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            dataStoreManager.clearSelectedLessons()
+                        }
+                        showDialog = false
+                    }
+                ) {
+                    Text("Yes")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { showDialog = false }) {
+                    Text("No")
+                }
+            }
+        )
     }
 }
 
